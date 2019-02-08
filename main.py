55fac76875801255fca51a461eca965751fedffb
alecokas <alecokastanos@gmail.com>
2019-02-08 17:17:01 +0000

    Reduced the epoch num, increased the learning rate, and corrected the set_experiment() function

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ from viz.visualize import Visualizer
 
 
 def default_experiment():
-    return {'epochs': 100000,
+    return {'epochs': 100,
             'batch_size': 64,
             'no_cuda': False,
             'seed': 1234,
             'log-level': "info",
-            "lr": 5e-4,
+            "lr": 1e-3,
             "capacity": [0.0, 5.0, 25000, 30.0],
             "print_every": 50,
             "record_every": 5,
@@ -39,33 +39,32 @@ def set_experiment(default_config):
     """ Set up default experiments to replicate the results in the paper:
         "Understanding Disentanglement in Beta-VAE" (https://arxiv.org/pdf/1804.03599.pdf)
     """
-    if default_config['experiment'] == 'custom':
+    if default_config.experiment == 'custom':
         return default_config
-    elif default_config['experiment'] == 'vae_blob_x_y':
-        default_config['capacity'] = 1
-        default_config['dataset'] = 'dsprites'
-    elif default_config['experiment'] == 'beta_vae_blob_x_y':
-        default_config['capacity'] = 150
-        default_config['dataset'] = 'black_and_white_dsprite'
-    elif default_config['experiment'] == 'beta_vae_dsprite':
-        default_config['capacity'] = [0.0, 25.0, 100000, 1000.0]
-        default_config['dataset'] = 'dsprites'
-    elif default_config['experiment'] == 'beta_vae_celeba':
-        default_config['capacity'] = [0.0, 50.0, 100000, 1000]
-        default_config['dataset'] = 'celeba'
-    elif default_config['experiment'] == 'beta_vae_colour_dsprite':
-        default_config['capacity'] = [0.0, 25.0, 100000, 1000]
-        default_config['dataset'] = 'dsprites'
-    elif default_config['experiment'] == 'beta_vae_chairs':
-        default_config['capacity'] = 1000
-        default_config['dataset'] = 'chairs'
+    elif default_config.experiment == 'vae_blob_x_y':
+        default_config.capacity = 1
+        default_config.dataset = 'dsprites'
+    elif default_config.experiment == 'beta_vae_blob_x_y':
+        default_config.capacity = 150
+        default_config.dataset = 'black_and_white_dsprite'
+    elif default_config.experiment == 'beta_vae_dsprite':
+        default_config.capacity = [0.0, 25.0, 100000, 1000.0]
+        default_config.dataset = 'dsprites'
+    elif default_config.experiment == 'beta_vae_celeba':
+        default_config.capacity = [0.0, 50.0, 100000, 10000.]
+        default_config.dataset = 'celeba'
+    elif default_config.experiment == 'beta_vae_colour_dsprite':
+        default_config.capacity = [0.0, 25.0, 100000, 1000.0]
+        default_config.dataset = 'dsprites'
+    elif default_config.experiment == 'beta_vae_chairs':
+        default_config.capacity = 1000
+        default_config.dataset = 'chairs'
 
     return default_config
 
 
 def parse_arguments():
     default_config = default_experiment()
-    experiment_config = set_experiment(default_config)
 
     parser = argparse.ArgumentParser(description="PyTorch implementation and evaluation of disentangled Variational AutoEncoders.",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -74,46 +73,46 @@ def parse_arguments():
     general = parser.add_argument_group('General options')
     log_levels = ['critical', 'error', 'warning', 'info', 'debug']
     general.add_argument('-L', '--log-level', help="Logging levels.",
-                         default=experiment_config['log-level'],
+                         default=default_config['log-level'],
                          choices=log_levels)
     parser.add_argument("-P", '--print_every',
-                        type=int, default=experiment_config['print_every'],
+                        type=int, default=default_config['print_every'],
                         help='Every how many batches to print results')
     parser.add_argument("-R", '--record_every',
-                        type=int, default=experiment_config['record_every'],
+                        type=int, default=default_config['record_every'],
                         help='Every how many batches to save results')
 
     # Dataset options
     data = parser.add_argument_group('Dataset options')
     datasets = ['mnist', "celeba", "chairs", "dsprites", "fashion_mnist"]
     data.add_argument('-d', '--dataset', help="Path to training data.",
-                      default=experiment_config['dataset'], choices=datasets)
+                      default=default_config['dataset'], choices=datasets)
 
     # Predefined experiments
     experiment = parser.add_argument_group('Predefined experiments')
     experiments = ['custom', 'vae_blob_x_y', 'beta_vae_blob_x_y', 'beta_vae_dsprite', 'beta_vae_celeba', 'beta_vae_colour_dsprite', 'beta_vae_chairs']
     experiment.add_argument('-x', '--experiment',
-                            default=experiment_config['experiment'], choices=experiments,
+                            default=default_config['experiment'], choices=experiments,
                             help='Predefined experiments to run. If not `custom` this will set the correct other arguments.')
 
     # Learning options
     learn = parser.add_argument_group('Learning options')
     learn.add_argument('-e', '--epochs',
-                       type=int, default=experiment_config['epochs'],
+                       type=int, default=default_config['epochs'],
                        help='Maximum number of epochs to run for.')
     learn.add_argument('-b', '--batch-size',
-                       type=int, default=experiment_config['batch_size'],
+                       type=int, default=default_config['batch_size'],
                        help='Batch size for training.')
-    learn.add_argument('-s', '--seed', type=int, default=experiment_config['seed'],
+    learn.add_argument('-s', '--seed', type=int, default=default_config['seed'],
                        help='Random seed. Can be `None` for stochastic behavior.')
     learn.add_argument('--no-cuda', action='store_true',
-                       default=experiment_config['no_cuda'],
+                       default=default_config['no_cuda'],
                        help='Disables CUDA training, even when have one.')
     learn.add_argument('-l', '--lr',
-                       type=float, default=experiment_config['lr'],
+                       type=float, default=default_config['lr'],
                        help='Learning rate.')
     learn.add_argument('-c', '--capacity',
-                       type=float, default=experiment_config['capacity'],
+                       type=float, default=default_config['capacity'],
                        metavar=('MIN_CAPACITY, MAX_CAPACITY, NUM_ITERS, GAMMA_Z'),
                        nargs='*',
                        help="Capacity of latent channel.")
@@ -122,13 +121,17 @@ def parse_arguments():
     model = parser.add_argument_group('Learning options')
     models = ['Burgess']
     model.add_argument('-m', '--model',
-                       default=experiment_config['model'], choices=models,
+                       default=default_config['model'], choices=models,
                        help='Type of encoder and decoder to use.')
     model.add_argument('-z', '--latent-dim',
-                       default=experiment_config['latent_dim'], type=int,
+                       default=default_config['latent_dim'], type=int,
                        help='Dimension of the latent variable.')
 
     args = parser.parse_args()
+    
+    print(args)
+
+    experiment_config = set_experiment(args)
 
     return args
 
@@ -201,4 +204,5 @@ def main(args):
 
 if __name__ == '__main__':
     args = parse_arguments()
+    args.experiment
     main(args)
