36fe6197c7a2774d2608f305c64d19bcf573993d
alecokas <alecokastanos@gmail.com>
2019-03-05 17:32:32 +0000

    Can choose to save or display the Avg KL per latent dim

--- create_plots.py
+++ create_plots.py
@@ -9,14 +9,14 @@ def parse_arguments():
     # General options
     dir_opts = parser.add_argument_group('directory options')
     dir_opts.add_argument('-l', '--log_dir', help='Path to the log file containing the data to plot.', required=True)
-    dir_opts.add_argument('-o', '--output_dir', help='The directory in which to save the plot.')
+    dir_opts.add_argument('-o', '--output_file_name', help='The full path name to use when saving the plot.')
 
     args = parser.parse_args()
     return args
 
 
 def main(args):
-    LogPlotter(log_dir=args.log_dir, output_dir=args.output_dir)
+    LogPlotter(log_dir=args.log_dir, output_file_name=args.output_file_name)
 
 
 if __name__ == '__main__':
