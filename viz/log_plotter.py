4c366ef1da95248cd75cd6596fa885404ad1a5a9
B.J. Melman <bjm62@mlsalt-cpu2.eng.cam.ac.uk>
2019-03-27 15:14:36 +0000

    revert file viz/log_plotter.py back to master

--- viz/log_plotter.py
+++ viz/log_plotter.py
@@ -2,16 +2,15 @@ import matplotlib
 matplotlib.use('AGG')
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
 import os
 
 class LogPlotter(object):
     """ Definition of a class for reading in a log file and plotting the results.
     """
-    def __init__(self, log_dir, output_file_name='imgs/avg_kl_per_factor.png', pdf=False):
+    def __init__(self, log_dir, output_file_name=None, pdf=False):
         """ Initialise log plot """
         plot = self.plot(log_dir)
-        print('start plotting yes')
+
         if output_file_name is None:
             plot.show()
         else:
@@ -27,18 +26,10 @@ class LogPlotter(object):
         """ Display the content of the log file as a line plot. """
         if not os.path.isfile(log_dir):
             raise Exception('Please ensure that you enter log file path using \'-l\'')
-        print('start plotting')
+
         df = pd.read_csv(log_dir)
-        unique_names = df.Loss.unique()
-        y = df.loc[df['Loss'] == unique_names[0]].Value
-        headers = unique_names
-        x_axis = df.Epoch.unique()
+        headers = list(df)[1:]
+        x_axis = list(df)[0]
+        df.plot(x=x_axis, y=headers, grid=True, style='-')
 
-        y = np.zeros((len(y),len(unique_names)))
-        for i in range(0, len(unique_names)):
-            name = unique_names[i]
-            y[:,i]=df.loc[df['Loss'] == unique_names[i]].Value
-        print(y)
-        df.plot(x=x_axis, y=df.loc[df['Loss'] == unique_names[0]].Value, grid=True, style='-')
-
-        return plt
+return plt
