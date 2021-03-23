7875783548738db2db04cf812de4aef06768cb10
Matthew Morris <matthewthemorris@gmail.com>
2021-03-23 14:20:59 +0000

    Added CIFAR10. Fixed MNIST links

--- utils/datasets.py
+++ utils/datasets.py
@@ -22,7 +22,8 @@ DATASETS_DICT = {"mnist": "MNIST",
                  "fashion": "FashionMNIST",
                  "dsprites": "DSprites",
                  "celeba": "CelebA",
-                 "chairs": "Chairs"}
+                 "chairs": "Chairs",
+                 "cifar": "CIFAR10"}
 DATASETS = list(DATASETS_DICT.keys())
 
 
@@ -323,13 +324,13 @@ class Chairs(datasets.ImageFolder):
         self.transforms = transforms.Compose([transforms.Grayscale(),
                                               transforms.ToTensor()])
         self.logger = logger
-
+        
         if not os.path.isdir(root):
             self.logger.info("Downloading {} ...".format(str(type(self))))
             self.download()
             self.logger.info("Finished Downloading.")
 
-        super().__init__(self.train_data, transform=self.transforms)
+        super().__init__(root=self.train_data, transform=self.transforms)
 
     def download(self):
         """Download the dataset."""
@@ -357,6 +358,14 @@ class MNIST(datasets.MNIST):
     img_size = (1, 32, 32)
     background_color = COLOUR_BLACK
 
+    # Manually specify download links
+    resources = [
+        ("https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz", "f68b3c2dcbeaaa9fbdd348bbdeb94873"),
+        ("https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz", "d53e105ee54ea40749a09fcbcd1e9432"),
+        ("https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz", "9fb629c4189551a2d022fa330f9573f3"),
+        ("https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz", "ec29112dd5afa0611ce80d1b7f02629c")
+    ]
+
     def __init__(self, root=os.path.join(DIR, '../data/mnist'), **kwargs):
         super().__init__(root,
                          train=True,
@@ -366,6 +375,19 @@ class MNIST(datasets.MNIST):
                              transforms.ToTensor()
                          ]))
 
+class CIFAR10(datasets.CIFAR10):
+    """CIFAR10 wrapper"""
+    img_size = (3, 32, 32)
+    background_color = COLOUR_BLACK
+    
+    def __init__(self, root=os.path.join(DIR, '../data/cifar10'), **kwargs):
+        super().__init__(root,
+                         train=True,
+                         download=True,
+                         transform=transforms.Compose([
+                             transforms.Resize(32),
+                             transforms.ToTensor()
+                         ]))
 
 class FashionMNIST(datasets.FashionMNIST):
     """Fashion Mnist wrapper. Docs: `datasets.FashionMNIST.`"""
