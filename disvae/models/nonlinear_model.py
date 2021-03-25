2bb6a1d74d845275655848cac215df7b0d3b609c
Munib <mm8625@nyu.edu>
2021-03-25 13:50:42 +0100

    nonlinear model

---
+++ disvae/models/nonlinear_model.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+
+class NonLinearModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(LinearModel, self).__init__()
+        
+        # Fully connected layer
+        self.lin1 = nn.Linear(input_dim, hidden_dim)
+        self.lin2 = nn.Linear(hidden_dim, output_dim)
+        self.log_softmax = torch.nn.LogSoftmax() #correct dimension? dim =1 gives negative NLL
+
+    def forward(self, x):
+        return self.log_softmax(nn.Sigmoid(self.lin2(nn.Sigmoid(self.lin1(x)))))
+
+def weight_reset(m):
+    if isinstance(m, nn.Linear):
+        m.reset_parameters()
+        
