4641cdc45ac117e9921037469620c686595657bb
Mirofil <kawgawr@gmail.com>
2021-03-31 21:37:31 +0200

    Some missing options

--- main.py
+++ main.py
@@ -71,7 +71,10 @@ def parse_arguments(args_to_parse):
                         help='List of indices to of images to put at the begining of the samples.')
     general.add_argument("plots", type=str, nargs='+', choices=PLOT_TYPES, default="all",
                         help="List of all plots to generate. `generate-samples`: random decoded samples. `data-samples` samples from the dataset. `reconstruct` first rnows//2 will be the original and rest will be the corresponding reconstructions. `traversals` traverses the most important rnows dimensions with ncols different samples from the prior or posterior. `reconstruct-traverse` first row for original, second are reconstructions, rest are traversals. `gif-traversals` grid of gifs where rows are latent dimensions, columns are examples, each gif shows posterior traversals. `all` runs every plot.")
-
+    general.add_argument('-r', '--n-rows', type=int, default=6,
+                        help='The number of rows to visualize (if applicable).')
+    general.add_argument('-c', '--n-cols', type=int, default=7,
+                        help='The number of columns to visualize (if applicable).')
     # Learning options
     training = parser.add_argument_group('Training specific options')
     training.add_argument('--checkpoint-every',
