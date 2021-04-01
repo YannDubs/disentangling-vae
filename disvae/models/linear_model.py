50dd1917a65f093105d68a1613e92f7b3e107ea6
Mirofil <kawgawr@gmail.com>
2021-04-01 18:21:39 +0200

    Trying Jonas's old model definition

--- disvae/models/linear_model.py
+++ disvae/models/linear_model.py
@@ -1,22 +1,21 @@
 import torch
 from torch import nn
 
-class Classifier(nn.Module):
+
+class LinearModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, use_non_linear):
-        super(Classifier, self).__init__()
+        super(LinearModel, self).__init__()
         self.use_non_linear = use_non_linear
 
         # Fully connected layer
+        self.lin1 = nn.Linear(input_dim, hidden_dim)
+        self.lin2 = nn.Linear(hidden_dim, hidden_dim)
+        self.lin3 = nn.Linear(hidden_dim, output_dim)
+        #self.lin4 = nn.Linear(int(hidden_dim/2), output_dim)
         if use_non_linear:
-            self.lin1 = nn.Linear(input_dim, hidden_dim)
-            self.lin2 = nn.Linear(hidden_dim, hidden_dim)
-            self.lin3 = nn.Linear(hidden_dim, output_dim)
-            #self.lin4 = nn.Linear(int(hidden_dim/2), output_dim)
             self.act1 = nn.ReLU()
             self.act2 = nn.ReLU()
             #self.act3 = nn.ReLU()
-        else:
-            self.lin1 = nn.Linear(input_dim, output_dim)
         self.log_softmax = torch.nn.LogSoftmax(dim=1)
 
 
@@ -24,9 +23,10 @@ class Classifier(nn.Module):
         x = self.lin1(x)
         if self.use_non_linear:
             x = self.act1(x)
-            x = self.lin2(x)
+        x = self.lin2(x)
+        if self.use_non_linear:
             x = self.act2(x)
-            x = self.lin3(x)
+        x = self.lin3(x)
         #if self.use_non_linear:
         #    x = self.act3(x)
         #x = self.lin4(x)
@@ -38,3 +38,41 @@ class Classifier(nn.Module):
 def weight_reset(m):
     if isinstance(m, nn.Linear):
         m.reset_parameters()
+
+# class Classifier(nn.Module):
+#     def __init__(self, input_dim, hidden_dim, output_dim, use_non_linear):
+#         super(Classifier, self).__init__()
+#         self.use_non_linear = use_non_linear
+
+#         # Fully connected layer
+#         if use_non_linear:
+#             self.lin1 = nn.Linear(input_dim, hidden_dim)
+#             self.lin2 = nn.Linear(hidden_dim, hidden_dim)
+#             self.lin3 = nn.Linear(hidden_dim, output_dim)
+#             #self.lin4 = nn.Linear(int(hidden_dim/2), output_dim)
+#             self.act1 = nn.ReLU()
+#             self.act2 = nn.ReLU()
+#             #self.act3 = nn.ReLU()
+#         else:
+#             self.lin1 = nn.Linear(input_dim, output_dim)
+#         self.log_softmax = torch.nn.LogSoftmax(dim=1)
+
+
+#     def forward(self, x):
+#         x = self.lin1(x)
+#         if self.use_non_linear:
+#             x = self.act1(x)
+#             x = self.lin2(x)
+#             x = self.act2(x)
+#             x = self.lin3(x)
+#         #if self.use_non_linear:
+#         #    x = self.act3(x)
+#         #x = self.lin4(x)
+#         #x = self.lin3(x)
+#         #if self.use_non_linear:
+#         #    x = self.act3(x)
+#         return self.log_softmax(x)
+
+# def weight_reset(m):
+#     if isinstance(m, nn.Linear):
+#         m.reset_parameters()
