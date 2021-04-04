5726f6b6c314bea5cbdea737578d3a77f6fb3754
Mirofil <kawgawr@gmail.com>
2021-04-04 15:31:55 +0200

    Better PCA size

--- disvae/evaluate.py
+++ disvae/evaluate.py
@@ -246,7 +246,7 @@ class Evaluator:
                     imgs_pca = np.reshape(data_imgs, (data_imgs.shape[0], data_imgs.shape[1]**2))
                 else: 
                     imgs_pca = np.reshape(dataset.imgs, (dataset.imgs.shape[0], dataset.imgs.shape[1]**2))
-                size = min(25000, len(imgs_pca))
+                size = min(500000, len(imgs_pca))
 
                 idx = np.random.randint(len(imgs_pca), size = size)
                 imgs_pca = imgs_pca[idx, :]       #not enough memory for full dataset -> repeat with random subsets               
